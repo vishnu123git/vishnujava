970362c49f5ed5cd9b86c1da3eeb9e714c5db972
vishnu123git <159704520+vishnu123git@users.noreply.github.com>
2024-03-17 10:47:13 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,51 @@
+let boy = 50;
+
+if(boy>=50){
+    console.log("passed");
+    
+
+}
+
+else{
+    console.log("failed");
+}
+
+
+let girl = 40;
+
+if(girl>=50){
+    console.log("passed");
+    
+
+}
+
+else{
+    console.log("failed");
+}
+
+
+
+let ottuser = 18;
+
+if(ottuser>=18){
+
+    console.log("submit");
+}
+
+else{
+    console.log("not submit");
+}
+
+
+let normal = 16;
+
+if(normal>=18){
+
+    console.log("submit");
+}
+
+else{
+    console.log("not submit");
+}
+
+console.log(10**2);
